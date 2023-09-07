ba868bc4bf33c19a339bd909072a4be8ea59b7ce
Klim Sukhikh <40332350+sirmong@users.noreply.github.com>
2023-09-07 22:27:19 +0200

    feat: created app container

--- app/src/main/java/com/example/marsphotos/data/MarsPhotosRepository.kt
+++ app/src/main/java/com/example/marsphotos/data/MarsPhotosRepository.kt
@@ -1,14 +1,15 @@
 package com.example.marsphotos.data
 
-import com.example.marsphotos.network.MarsApi
+import com.example.marsphotos.network.MarsApiService
 import com.example.marsphotos.network.MarsPhoto
 
 interface MarsPhotosRepository {
     suspend fun getMarsPhotos(): List<MarsPhoto>
 }
 
-class NetworkMarsPhotoRepository() : MarsPhotosRepository {
+class NetworkMarsPhotoRepository(private val marsApiService: MarsApiService) :
+    MarsPhotosRepository {
     override suspend fun getMarsPhotos(): List<MarsPhoto> {
-        return MarsApi.retrofitService.getPhotos()
+        return marsApiService.getPhotos()
     }
 }
